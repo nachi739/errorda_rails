ab7bc1130068601c8b0da060b8a38fe46a463618
Yuichi Namiki <namiki081025@gmail.com>
2022-04-06 13:42:19 +0900

    [add]model　テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) {FactoryBot.create(:user)}
   before do
     @user = build(:user)
   end
-
-  describe ' test' do
-    it ' test' do
-      expect(@user)
+  describe 'ユーザーmodelについてのテスト' do
+    context '保存後' do
+      it 'レコードが０ではないか' do
+        expect(User.exists?).to eq true
+      end
+      it 'nameの値の保存の確認' do
+        expect(@user.name).to eq 'test'
+      end
+      it 'idの確認' do
+        expect(@user.id).to eq @user.id
+      end
+      it 'created_atの確認' do
+        expect(@user.created_at).to eq @user.created_at
+      end
+      it 'updated_atの確認' do
+        expect(@user.updated_at).to eq @user.updated_at
+      end
     end
+
   end
+
 end
+
