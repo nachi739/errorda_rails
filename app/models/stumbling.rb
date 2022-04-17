35104c9919b054aa79c5c08707870ae10a35979a
Yuichi Namiki <namiki081025@gmail.com>
2022-04-17 19:08:02 +0900

    [update]コメント追記

--- app/models/stumbling.rb
+++ app/models/stumbling.rb
@@ -2,6 +2,7 @@ class Stumbling < ApplicationRecord
 
     belongs_to :user, primary_key: :name, foreign_key: :user_name #userテーブルと１対多の関係(stumblingが多)
 
+    #今後end_timeメソッドのDB処理をこちらに追記する
 end
 
 #ActiveRecordクラスはDBとやり取りする際に必要なSQL構文を翻訳する機能を持っている。
